01cb196f9e7c834b22961fede7b5ce1538232847
krkaiser <kenny.kaiser@gmail.com>
2011-02-27 11:13:23 -0500

    Added user following

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -60,15 +60,15 @@ describe Micropost do
     end
 
     it "should include the followed user's microposts" do
-      Microposts.from_users_followed_by(@user).should include(@other_post)
+      Micropost.from_users_followed_by(@user).should include(@other_post)
     end
 
     it "should include the user's own microposts" do
-      Microposts.from_users_followed_by(@user).should include(@other_post)
+      Micropost.from_users_followed_by(@user).should include(@other_post)
     end
 
     it "should not include an unfollowed user's microposts" do
-      Microposts.from_users_followed_by(@user).should_not include(@third_post)
+      Micropost.from_users_followed_by(@user).should_not include(@third_post)
     end
 
   end
